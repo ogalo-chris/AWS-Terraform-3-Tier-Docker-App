015fa2f78baa0acf0d5e4cf30c793d277f30ee35
ogalo-chris <ogalo.chris21@gmail.com>
2026-04-02 08:15:23 +0300

    "Updated the Postgres egine version to 15"

--- terraform-infra/modules/rds/variables.tf
+++ terraform-infra/modules/rds/variables.tf
@@ -33,7 +33,7 @@ variable "allocated_storage" {
 variable "engine_version" {
   description = "PostgreSQL engine version"
   type        = string
-  default     = "15.5"
+  default     = "15"
 }
 
 variable "db_name" {
